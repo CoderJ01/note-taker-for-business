dbcb463454fbd434295ac1a5b62ac8596ea680ef
Coder J <buildingguy@sbcglobal.net>
2022-02-02 16:30:18 -0800

    set request for single note

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ app.get('/api/notes', (req, res) => {
 });
 
 // GET request for a single note
+app.get('/api/notes/:notes_id', (req, res) => {
+    if(req.body && req.parameters.notes_id) {
+        console.info(`${req.method} request was recieved to get a single note`);
+        const noteID = req.parameters.notes_id;
+        for (let i = 0; i < notes.length; i++) {
+            const currentNote = notes[i];
+            if (currentNote.notes_id === noteID) {
+                res.json(currentNote);
+                return;
+            }
+        }
+        res.json('Note not found');
+    }
+});
 
 // POST request to add notes
 app.post('/api/notes', (req, res) => {
