d63e72af27cf470be9704ab2610665956d16abd9
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-11 17:16:10 -0800

    require path

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
