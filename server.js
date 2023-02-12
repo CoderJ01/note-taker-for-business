2f3ddca9030c284ba7ed5d42e28498c5e6017e16
CoderJ01 <buildingguy@sbcglobal.net>
2023-02-11 17:24:05 -0800

    testing for Vercel: uncomment other routes

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 const path = require('path');
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
 // Use apiRoutes
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
